aa8e8d6b4cf18260420bd47add209f5a336ce633
Thomas <thmasky@amazon.com>
2019-09-03 17:13:11 -0400

    Initial Commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,46 @@
+let teamOneButton = document.querySelector ("#teamone-shoot");
+let teamOneShotCounter = document.querySelector ("#teamone-numshots")
+let teamOneGoals = document.querySelector ("#teamone-numhits");
+
+teamOneButton.addEventListener ("click", function()
+{
+    teamOneShotCounter.innerHTML = parseInt(teamOneShotCounter.innerHTML) + 1;
+
+    if (Math.random() < .25)
+    {
+        teamOneGoals.innerHTML = parseInt(teamOneGoals.innerHTML) + 1;
+    }
+})
+
+
+let teamTwoButton = document.querySelector ("#teamtwo-shoot");
+let teamTwoShotCounter = document.querySelector ("#teamtwo-numshots")
+let teamTwoGoals = document.querySelector ("#teamtwo-numhits");
+
+teamTwoButton.addEventListener ("click", function()
+{
+    teamTwoShotCounter.innerHTML = parseInt(teamTwoShotCounter.innerHTML) + 1;
+
+    if (Math.random() < .25)
+    {
+        teamTwoGoals.innerHTML = parseInt(teamTwoGoals.innerHTML) + 1;
+    }
+})
+
+
+let resetButton = document.querySelector ("#reset");
+let resetCounter = document.querySelector ("#num-resets");
+
+resetButton.addEventListener ("click", function()
+{
+
+    teamOneShotCounter.innerHTML = 0;
+    teamOneGoals.innerHTML = 0;
+    teamTwoShotCounter.innerHTML = 0;
+    teamTwoGoals.innerHTML = 0;
+
+    
+
+
+    resetCounter.innerHTML = parseInt(resetCounter.innerHTML) + 1; 
+})
